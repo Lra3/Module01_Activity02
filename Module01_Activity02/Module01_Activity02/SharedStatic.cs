382f460f51bad784b830dad7891c396977cad4f3
nakpi <nakpi@DESKTOP-C0VPPF3>
2022-09-12 20:10:36 +0800

    Add project files.

---
+++ Module01_Activity02/Module01_Activity02/SharedStatic.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Module01_Activity02
+{
+    public class SharedStatic
+    {
+        public static readonly Color BColor = Color.Black;
+        public static readonly Color BTColor = Color.White;
+
+    }
+}
